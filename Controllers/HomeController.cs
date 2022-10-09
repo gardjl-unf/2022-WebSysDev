34d5fba6e4d67d63163afc5a180261c640191e89
Jason Gardner <n01480000@unf.edu>
2022-10-09 13:09:57 -0400

    MVC Outline

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace COP3855_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COP3855_Project.Models
+{
+    public class Vehicle : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
