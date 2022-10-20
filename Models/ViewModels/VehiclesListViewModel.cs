0b1ba45100b07ad14fa829e2cfee63b0c0c7eff3
Jason Gardner <77986968+gardjl-unf@users.noreply.github.com>
2022-10-20 11:30:05 -0400

    Tag Helper and Stuff

---
+++ Models/ViewModels/VehiclesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using COP3855_Project.Models;
+
+namespace COP3855_Project.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Vehicle> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
