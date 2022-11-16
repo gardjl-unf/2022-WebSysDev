7e53caa994ae4c10c56e3034197f08a4943f4157
Jason <n01480000@unf.edu>
2022-11-16 11:06:47 -0500

    New Car Ordering Workflow

--- Controllers/Vehicle.cs
+++ Controllers/Vehicle.cs
@@ -1,22 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Collections;
+using Tuskla.Models;
+using Tuskla.Models.ViewModels;
 
 namespace Tuskla.Controllers
 {
     public class Vehicle : Controller
     {
-        public IActionResult Index()
+        ArrayList vehicleItems = new ArrayList();
+        private IProductRepository repository;
+        public Vehicle(IProductRepository repo)
         {
-            return View();
+            repository = repo;
         }
-        public ViewResult Index(string model)
+        public void AddItem(Product product) => vehicleItems.Add(product);
+        public ViewResult Index(string Model)
         {
+            ViewBag.Logo = "../img/home/model-" + Model.Substring(6, 1).ToLower() + ".jpg";
+            ViewBag.ModelType = Model.Substring(0, 7);
+            ViewBag.ModelStandard = ViewBag.ModelType + " Standard";
+            ViewBag.ModelPlaid = ViewBag.ModelType + " Plaid";
 
-            ViewBag.Logo = "..//img//home-" + model.Substring(6, 1) + ".jpg";
-            ViewBag.ModelType = model.Substring(0, 7);
-            ViewBag.ModelButtonText = model.Split(' ').Last();
-            
-            return View("Design");
+            return View("Index");
+        }
+        public ViewResult Design(string Model)
+        {
+            ViewBag.ModelImage = "../img/" + Model.Substring(6, 1).ToLower() + "/ext/18/white/front.jpg";
+            ViewBag.ModelType = Model.Substring(0, 7);
+            ViewBag.ModelButtonText = Model.Split(' ').Last();
+
+
+            return View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => p.Category.StartsWith("Car") && p.isActive == true)
+                    .OrderBy(p => p.ProductID)
+            });
         }
     }
 }
