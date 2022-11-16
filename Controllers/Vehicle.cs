f2369438b95375215e294354db045b999bee316d
Jason Gardner <n01480000@unf.edu>
2022-11-16 18:24:10 -0500

    Broken Code

--- Controllers/Vehicle.cs
+++ Controllers/Vehicle.cs
@@ -3,18 +3,17 @@ using System.Linq;
 using System.Collections;
 using Tuskla.Models;
 using Tuskla.Models.ViewModels;
+using System.Data.Common;
 
 namespace Tuskla.Controllers
 {
-    public class Vehicle : Controller
+    public class VehicleController : Controller
     {
-        ArrayList vehicleItems = new ArrayList();
         private IProductRepository repository;
-        public Vehicle(IProductRepository repo)
+        public VehicleController(IProductRepository repo)
         {
             repository = repo;
         }
-        public void AddItem(Product product) => vehicleItems.Add(product);
         public ViewResult Index(string Model)
         {
             ViewBag.Logo = "../img/home/model-" + Model.Substring(6, 1).ToLower() + ".jpg";
@@ -29,6 +28,7 @@ namespace Tuskla.Controllers
             ViewBag.ModelImage = "../img/" + Model.Substring(6, 1).ToLower() + "/ext/18/white/front.jpg";
             ViewBag.ModelType = Model.Substring(0, 7);
             ViewBag.ModelButtonText = Model.Split(' ').Last();
+            ViewBag.ModelName = Model;
 
 
             return View(new ProductsListViewModel
@@ -37,6 +37,20 @@ namespace Tuskla.Controllers
                     .Where(p => p.Category.StartsWith("Car") && p.isActive == true)
                     .OrderBy(p => p.ProductID)
             });
+        }
+        [HttpPost]
+        public ViewResult Order(string Model)
+        {
+            Cart.AddItem(repository.Products.Where(p => p.Name == Model), 1);
+            foreach (int productID in Request.Query.ToArray)
+            {
+                foreach (Product product in repository.Products.Where(p => p.ProductID == productID))
+                {
+                    Cart.AddItem(product, 1);
+                }
+            }
+
+            return View(Design(Model));
         }
     }
 }
