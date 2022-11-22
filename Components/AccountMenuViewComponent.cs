497269759b2d5a216559fa50bfeb1facfbd8c3fc
Jason <n01480000@unf.edu>
2022-11-22 10:06:54 -0500

    Account Menu View Component

---
+++ Components/AccountMenuViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Tuskla.Components
+{
+    public class AccountMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
