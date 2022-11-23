a4daea607fd39bd63d794dafc8f84fa0f82972d2
Jason <n01480000@unf.edu>
2022-11-23 09:37:36 -0500

    all Changes are undone

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -107,7 +107,7 @@ namespace Tuskla.Controllers
         {
             cart.Clear();
             ViewBag.orderid = repository.Orders.Max(o => o.OrderID);
-            
+
             return View();
         }
 
