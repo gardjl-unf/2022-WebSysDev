ea214ced006c1c4bd17a95f3155cea9c2881e6b2
Jason <n01480000@unf.edu>
2022-11-25 11:43:23 -0500

    Checkout Page JS

--- wwwroot/js/pay.js
+++ wwwroot/js/pay.js
@@ -1,17 +1,9 @@
-﻿$(function () {
-
-    var owner = $('#owner');
-    var cardNumber = $('#cardNumber');
-    var cardNumberField = $('#card-number-field');
-    var CVV = $("#cvv");
-    var mastercard = $("#mastercard");
-    var confirmButton = $('#confirm-purchase');
-    var visa = $("#visa");
-    var amex = $("#amex");
-
-    // Use the payform library to format and validate
-    // the payment fields.
-
-    cardNumber.payform('formatCardNumber');
-    CVV.payform('formatCardCVC');
-})
+﻿$(document).ready(function () {
+    $(".bankPayment").hide();
+    $('input[type="radio"]').click(function () {
+        var inputValue = $(this).attr("value");
+        var targetBox = $("." + inputValue);
+        $(".box").not(targetBox).hide();
+        $(targetBox).show();
+    });
+});
