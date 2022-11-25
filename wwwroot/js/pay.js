77efb821f96d0e7afa8d5274ae54f3ba41315b7e
Jason <n01480000@unf.edu>
2022-11-25 16:06:27 -0500

    Checkout Page

--- wwwroot/js/pay.js
+++ wwwroot/js/pay.js
@@ -1,12 +1,20 @@
 ﻿$(document).ready(function () {
     $(".bankPayment").hide();
+    $(".paymentDeposit").hide();
     $('input[type="radio"]').click(function () {
         var inputValue = $(this).attr("value");
         var targetBox = $("." + inputValue);
         $(".box").not(targetBox).hide();
         $(targetBox).show();
     });
-    $('input[type="checkbox"]').click(function () {
-        
-    })
+    $('input[id="Deposit"]').click(function () {
+        if ($(this).prop("checked") == true) {
+            $(".paymentFull").hide();
+            $(".paymentDeposit").show();
+        }
+        else {
+            $(".paymentDeposit").hide();
+            $(".paymentFull").show();
+        }
+    });
 });
